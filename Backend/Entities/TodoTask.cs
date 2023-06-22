9b27b74400731c16221e6c270183e3ab2b9ca4ee
zabinskas.paulius951@gmail.com <zabinskas.paulius951@gmail.com>
2023-06-22 18:36:42 +0100

    Server side data validation added

--- Backend/Entities/TodoTask.cs
+++ Backend/Entities/TodoTask.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Backend.Entities;
 
 public class TodoTask
 {
-    public Guid  Id { get; set; }
+    public Guid Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
     public required string TaskName { get; set; } 
     public required string Discription { get; set; }
     
